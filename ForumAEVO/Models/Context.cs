786c8884eaccafe9b517432b360dc2d66141b77f
Tadeu Junior <tadeu_junior@outlook.com>
2023-09-10 20:07:08 -0300

    Configurando API para deploy no azure

---
+++ ForumAEVO/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ForumAEVO.Models
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+            /*Database.EnsureCreated();*/
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Topico> Topicos { get; set; }
+
+    }
+}
